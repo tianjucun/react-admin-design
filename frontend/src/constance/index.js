bd7775f3586178f5ea277331bcc45298491dca01
tianjucun <1670426063@qq.com>
2025-12-22 11:16:53 +0800

    feat: 添加菜单类型常量定义
    - 新增 MenuType 枚举常量
    
    - 新增 MenuTypeLabel 标签映射
    
    - 统一管理菜单类型相关常量

---
+++ frontend/src/constance/index.js
@@ -0,0 +1,11 @@
+export const MenuType = {
+  DIRECTORY: 1,
+  MENU: 2,
+  BUTTON: 3,
+};
+
+export const MenuTypeLabel = {
+  [MenuType.DIRECTORY]: '目录',
+  [MenuType.MENU]: '菜单',
+  [MenuType.BUTTON]: '按钮',
+};
